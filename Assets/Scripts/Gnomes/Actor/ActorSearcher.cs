67a5f3f9c22a0f264eb2d20abd18065f376f4aeb
Tyler Sims <tylursims@gmail.com>
2025-03-29 20:55:35 -0500

    Adding GNOMES submodule

--- Assets/Scripts/Gnomes/Actor/ActorSearcher.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-namespace Gnomes.Actor {
-    public static class ActorSearcher
-    {
-        private static Dictionary<int,List<Actor>> actors;
-    }
-}
--- Assets/Scripts/Gnomes/Interfaces/IDamageable.cs
+++
@@ -1,5 +0,0 @@
-namespace Gnomes.Interfaces {
-    public interface IDamageable {
-        public void TakeDamage(Actor.Actor attacker, float damage);       
-    }
-}
