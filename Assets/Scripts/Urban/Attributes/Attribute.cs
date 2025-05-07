1b133ae8841fa4594177420c3b9eca454599e4cc
Tyler Sims <tylursims@gmail.com>
2025-05-06 20:52:21 -0500

    Removing Urban to as submodule

--- Assets/Scripts/Urban/Attributes/Attribute.cs
+++
@@ -1,9 +0,0 @@
-namespace Urban.Attributes {
-    public class Attribute
-    {
-        public AttributeDetails Details;
-        public Attribute(AttributeDetails details) {
-            Details = details;
-        }
-    }
-}
